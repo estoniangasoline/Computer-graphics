e616485b9e641de21ffb2240a24fe97790737c20
estoniangasoline <ilovejessicanigri228@gmail.com>
2025-04-30 21:47:25 +0300

    lab 2 v0.1

---
+++ KGLab2/KGLab2/KGLab2.cpp
@@ -0,0 +1,226 @@
+﻿// KGLab2.cpp : Определяет точку входа для приложения.
+//
+
+#include "framework.h"
+#include "KGLab2.h"
+#include <chrono>
+#include <thread>
+
+#define MAX_LOADSTRING 100
+
+// Глобальные переменные:
+HINSTANCE hInst;                                // текущий экземпляр
+WCHAR szTitle[MAX_LOADSTRING];                  // Текст строки заголовка
+WCHAR szWindowClass[MAX_LOADSTRING];            // имя класса главного окна
+
+// Отправить объявления функций, включенных в этот модуль кода:
+ATOM                MyRegisterClass(HINSTANCE hInstance);
+BOOL                InitInstance(HINSTANCE, int);
+LRESULT CALLBACK    WndProc(HWND, UINT, WPARAM, LPARAM);
+INT_PTR CALLBACK    About(HWND, UINT, WPARAM, LPARAM);
+
+int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
+                     _In_opt_ HINSTANCE hPrevInstance,
+                     _In_ LPWSTR    lpCmdLine,
+                     _In_ int       nCmdShow)
+{
+    UNREFERENCED_PARAMETER(hPrevInstance);
+    UNREFERENCED_PARAMETER(lpCmdLine);
+
+    // TODO: Разместите код здесь.
+
+    // Инициализация глобальных строк
+    LoadStringW(hInstance, IDS_APP_TITLE, szTitle, MAX_LOADSTRING);
+    LoadStringW(hInstance, IDC_KGLAB2, szWindowClass, MAX_LOADSTRING);
+    MyRegisterClass(hInstance);
+
+    // Выполнить инициализацию приложения:
+    if (!InitInstance (hInstance, nCmdShow))
+    {
+        return FALSE;
+    }
+
+    HACCEL hAccelTable = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDC_KGLAB2));
+
+    MSG msg;
+
+    // Цикл основного сообщения:
+    while (GetMessage(&msg, nullptr, 0, 0))
+    {
+        if (!TranslateAccelerator(msg.hwnd, hAccelTable, &msg))
+        {
+            TranslateMessage(&msg);
+            DispatchMessage(&msg);
+        }
+    }
+
+    return (int) msg.wParam;
+}
+
+
+
+//
+//  ФУНКЦИЯ: MyRegisterClass()
+//
+//  ЦЕЛЬ: Регистрирует класс окна.
+//
+ATOM MyRegisterClass(HINSTANCE hInstance)
+{
+    WNDCLASSEXW wcex;
+
+    wcex.cbSize = sizeof(WNDCLASSEX);
+
+    wcex.style          = CS_HREDRAW | CS_VREDRAW;
+    wcex.lpfnWndProc    = WndProc;
+    wcex.cbClsExtra     = 0;
+    wcex.cbWndExtra     = 0;
+    wcex.hInstance      = hInstance;
+    wcex.hIcon          = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_KGLAB2));
+    wcex.hCursor        = LoadCursor(nullptr, IDC_ARROW);
+    wcex.hbrBackground  = (HBRUSH)(COLOR_WINDOW+1);
+    wcex.lpszMenuName   = MAKEINTRESOURCEW(IDC_KGLAB2);
+    wcex.lpszClassName  = szWindowClass;
+    wcex.hIconSm        = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
+
+    return RegisterClassExW(&wcex);
+}
+
+//
+//   ФУНКЦИЯ: InitInstance(HINSTANCE, int)
+//
+//   ЦЕЛЬ: Сохраняет маркер экземпляра и создает главное окно
+//
+//   КОММЕНТАРИИ:
+//
+//        В этой функции маркер экземпляра сохраняется в глобальной переменной, а также
+//        создается и выводится главное окно программы.
+//
+BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
+{
+   hInst = hInstance; // Сохранить маркер экземпляра в глобальной переменной
+
+   HWND hWnd = CreateWindowW(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW,
+      CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, nullptr, nullptr, hInstance, nullptr);
+
+   if (!hWnd)
+   {
+      return FALSE;
+   }
+
+   ShowWindow(hWnd, nCmdShow);
+   UpdateWindow(hWnd);
+
+   return TRUE;
+}
+
+          //
+       //  ФУНКЦИЯ: WndProc(HWND, UINT, WPARAM, LPARAM)
+        //
+      //  ЦЕЛЬ: Обрабатывает сообщения в главном окне.
+      //
+   //  WM_COMMAND  - обработать меню приложения
+    //  WM_PAINT    - Отрисовка главного окна
+ //  WM_DESTROY  - отправить сообщение о выходе и вернуться
+  //
+//
+
+void catAnimation(HDC hdc, HDC memdc);
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    switch (message)
+    {
+    case WM_COMMAND:
+        {
+            int wmId = LOWORD(wParam);
+            // Разобрать выбор в меню:
+            switch (wmId)
+            {
+            case IDM_ABOUT:
+                DialogBox(hInst, MAKEINTRESOURCE(IDD_ABOUTBOX), hWnd, About);
+                break;
+            case IDM_EXIT:
+                DestroyWindow(hWnd);
+                break;
+            default:
+                return DefWindowProc(hWnd, message, wParam, lParam);
+            }
+        }
+        break;
+    case WM_PAINT:
+        {
+            PAINTSTRUCT ps;
+            HDC hdc = BeginPaint(hWnd, &ps);
+
+            HDC memdc;
+            memdc = CreateCompatibleDC(hdc);
+
+            HBITMAP bmw = (HBITMAP)LoadImage(NULL, L"newbed.bmp", IMAGE_BITMAP, 0, 0, LR_LOADFROMFILE);
+            SelectObject(memdc, bmw);
+            BitBlt(hdc, 0, 195, 743, 626, memdc, 0, 0, SRCCOPY);
+
+            std::thread thread(catAnimation, hdc, memdc);
+            
+            thread.join();
+            // TODO: Добавьте сюда любой код прорисовки, использующий HDC...
+            EndPaint(hWnd, &ps);
+        }
+        break;
+    case WM_DESTROY:
+        PostQuitMessage(0);
+        break;
+    default:
+        return DefWindowProc(hWnd, message, wParam, lParam);
+    }
+    return 0;
+}
+
+void catAnimation(HDC hdc, HDC memdc) {
+    HBITMAP catstatbmw = (HBITMAP)LoadImage(NULL, L"catstat.bmp", IMAGE_BITMAP, 0, 0, LR_LOADFROMFILE);
+    HBITMAP catleftbmw = (HBITMAP)LoadImage(NULL, L"catleft.bmp", IMAGE_BITMAP, 0, 0, LR_LOADFROMFILE);
+    HBITMAP catrightbmw = (HBITMAP)LoadImage(NULL, L"catright.bmp", IMAGE_BITMAP, 0, 0, LR_LOADFROMFILE);
+
+    while (true) {
+        SelectObject(memdc, catstatbmw);
+        BitBlt(hdc, 100, 0, 286, 268, memdc, 0, 0, SRCINVERT);
+        std::this_thread::sleep_for(std::chrono::nanoseconds(500000000));
+        BitBlt(hdc, 100, 0, 286, 268, memdc, 0, 0, SRCINVERT);
+
+
+        SelectObject(memdc, catleftbmw);
+        BitBlt(hdc, 100, 0, 286, 268, memdc, 0, 0, SRCINVERT);
+        std::this_thread::sleep_for(std::chrono::nanoseconds(500000000));
+        BitBlt(hdc, 100, 0, 286, 268, memdc, 0, 0, SRCINVERT);
+
+        SelectObject(memdc, catstatbmw);
+        BitBlt(hdc, 100, 0, 286, 268, memdc, 0, 0, SRCINVERT);
+        std::this_thread::sleep_for(std::chrono::nanoseconds(500000000));
+        BitBlt(hdc, 100, 0, 286, 268, memdc, 0, 0, SRCINVERT);
+
+
+        SelectObject(memdc, catrightbmw);
+        BitBlt(hdc, 100, 0, 286, 268, memdc, 0, 0, SRCINVERT);
+        std::this_thread::sleep_for(std::chrono::nanoseconds(500000000));
+        BitBlt(hdc, 100, 0, 286, 268, memdc, 0, 0, SRCINVERT);
+    }
+}
+
+// Обработчик сообщений для окна "О программе".
+INT_PTR CALLBACK About(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    UNREFERENCED_PARAMETER(lParam);
+    switch (message)
+    {
+    case WM_INITDIALOG:
+        return (INT_PTR)TRUE;
+
+    case WM_COMMAND:
+        if (LOWORD(wParam) == IDOK || LOWORD(wParam) == IDCANCEL)
+        {
+            EndDialog(hDlg, LOWORD(wParam));
+            return (INT_PTR)TRUE;
+        }
+        break;
+    }
+    return (INT_PTR)FALSE;
+}
